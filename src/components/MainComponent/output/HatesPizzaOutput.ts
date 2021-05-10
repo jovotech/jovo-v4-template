c882ad5afe6e3360b1ca41af05105575e4cecc05
Ruben <email@aegerter.tech>
2021-05-10 12:02:47 +0200

    :fire: Remove constructors from output

--- src/components/MainComponent/output/HatesPizzaOutput.ts
+++ src/components/MainComponent/output/HatesPizzaOutput.ts
@@ -1,8 +1,7 @@
-import { Jovo, BaseOutput } from '@jovotech/framework';
+import { BaseOutput } from '@jovotech/framework';
 
 export class HatesPizzaOutput extends BaseOutput {
-
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Output Template
     |--------------------------------------------------------------------------
@@ -11,13 +10,9 @@ export class HatesPizzaOutput extends BaseOutput {
     | Learn more here: www.jovo.tech/docs/output
     |
     */
-    build() {
-        return { 
-            message: 'That\'s OK! Not everyone likes pizza.',
-        }
-    };
-
-    constructor(jovo: Jovo) {
-        super(jovo);
-    }
+	build() {
+		return {
+			message: "That's OK! Not everyone likes pizza.",
+		};
+	}
 }
