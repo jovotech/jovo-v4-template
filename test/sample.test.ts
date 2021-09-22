65d0edb00f32ba3d32e45b7b52ad76b92505b805
Ruben <email@aegerter.tech>
2021-09-22 14:08:07 +0200

    :white_check_mark: Add sample unit tests

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,1 +1,15 @@
-expect(true).toBeTruthy();
+import { TestSuite, InputType } from '@jovotech/framework';
+
+test('should ask the user if they like pizza', async () => {
+  const testSuite = new TestSuite();
+
+  const { output } = await testSuite.run({
+    type: InputType.Launch, // or 'LAUNCH'
+  });
+
+  expect(output).toEqual([
+    {
+      message: 'Hello World! Do you like pizza?',
+    },
+  ]);
+});
